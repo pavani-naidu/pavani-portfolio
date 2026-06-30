d9f291f8cba1d0adf4bca2eb6e390eed5eb33628
Pavani Naidu <pavaninaidu76@gmail.com>
2026-06-30 10:34:07 +0530

    chore: move yuvimantra ai to a separate directory

--- yuvimantra ai/backend/dist/routes/feedbackRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const feedbackController_1 = require("../controllers/feedbackController");
-const auth_1 = require("../middleware/auth");
-const router = (0, express_1.Router)();
-// Allow authenticated submissions
-router.post('/', auth_1.authenticateToken, feedbackController_1.createFeedback);
-exports.default = router;
